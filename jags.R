f09ef840f59f59684db5264ab151ff79da137a79
Jonathan Dushoff <dushoff@mcmaster.ca>
2019-02-24 19:00:54 -0500

    Pushing from parent

---
+++ jags.R
@@ -0,0 +1,12 @@
+library("rjags")
+library("R2jags")
+library("readr")
+
+read_csv("http://biostat.mc.vanderbilt.edu/wiki/pub/Main/DataSets/FEV.csv")
+
+Bayesian_example.rmd
+Bayesian_example.rmd.rdeps
+Bayesian_regression_example.md
+Bayesian_statistics_Lecture_notes.md
+Bayesian_statistics.md
+
