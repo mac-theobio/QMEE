ee6e7c62341447be806dec40d272d04775c5d56b
Jonathan Dushoff <dushoff@mcmaster.ca>
2021-01-12 22:30:35 -0500

    Suddenly change docs/ logic

--- rweb.mk
+++ rweb.mk
@@ -21,18 +21,14 @@ html:
 md = $(wildcard *.md)
 rmd = $(wildcard *.rmd)
 mdh = $(md:.md=.html)
-mdhdocs = $(md:.md=.html.docs)
+mdhdocs = $(md:%.md=docs/%.html)
 
 update: $(mdhdocs)
 
 ######################################################################
 
 mds = pandoc --mathjax -s -c html/qmee.css -B html/header.html -A html/footer.html -o $@ $<
-%.html: %.md $(wildcard html/*.*)
+docs/%.html: %.md
 	$(MAKE) html
 	$(mds)
-
-%.docs: %
-	$(MAKE) docs
-	$(CP) $< docs
 
