65b6475190e176874b79a1eec22522807e8d23c8
Ben Bolker <bbolker@gmail.com>
2024-01-23 13:29:41 -0500

    BMB tweaks to live code ex

--- docs/code/viz24.R
+++ docs/code/viz24.R
@@ -1,4 +1,9 @@
 ## BB and JD don't really love the default grey background
+## In any case, you should pick the theme *you* like (or write
+##  your own: see e.g. https://emanuelaf.github.io/own-ggplot-theme.html)
+##  and set it at the top of your script so that all your plots inherit
+##  the same theme
+
 library(ggplot2); theme_set(theme_bw())
 library(performance)
 library(dotwhisker)
@@ -10,12 +15,18 @@ library(dotwhisker)
 ## ... maybe combine with method="lm"
 print(ggplot(mtcars)
 	+ aes(x=disp, y=mpg)
-	+ geom_point()
-	## + aes(color=cyl)
-	## + aes(color=as.factor(cyl))
-	## + geom_smooth(method="lm")
-	+ geom_smooth()
-)
+        + geom_point()
+      	## + aes(color=cyl)            ## numeric: colour ramp
+	## + aes(color=as.factor(cyl)) ## categorical: discrete colours
+	## + geom_smooth(method="lm")  ## regression line
+	+ geom_smooth()                ## loess (default)
+      )
+
+## if you want to plot a smooth line that's constrained to be positive or
+## between 0 and 1 (e.g. for proportion data), use a GLM (method = "glm")
+## or GAM (method = "gam") with the 'family' set to quasipoisson (for
+## positive) or quasibinomial (for proportions): see
+## https://github.com/mac-theobio/QMEE/blob/master/lectures/Contraception.rmd
 
 ###### Diagnostic
 m1 <- lm(mpg~disp, data=mtcars)
@@ -32,9 +43,14 @@ check_model(m1)
 m2 <- lm(mpg ~ cyl+disp+hp, data=mtcars)
 check_model(m2)
 
+## also see DHARMa package (we will discuss later)
+
 ###### Inferential
 
 ## This is super-weird – because we are comparing predictors with different units
+## omits the intercept by default (this is usually the right choice):
+##  show_intercept = TRUE if you really want it
+
 print(dwplot(m2))
 
 ## Scale the predictors for comparison, add a line for zero effect for reference
