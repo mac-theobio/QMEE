b33741c140de98d0d7a82c9e62eb660e1512f154
Jonathan Dushoff <dushoff@mcmaster.ca>
2024-03-28 15:06:10 -0400

    new file:   outputs/tidyMRE.Rout

--- docs/code/tidyMRE.R
+++ docs/code/tidyMRE.R
@@ -44,46 +44,16 @@ fit <- function(dat){
 simCIs <- function(simfun, fitfun, ...){
 	dat <- simfun(...)
 	fit <- fitfun(dat)
+	return(as.data.frame(tidy(fit, conf.int=TRUE)))
 	c <- as.data.frame(confint(fit))
-	return(data.frame(vname=rownames(c)
-		, est = c$Est
-		, low = c$"2.5"
-		, high = c$"97.5"
-	))
 }
 
 set.seed(101)
-nReps <- 1000
 
-print(simCIs(simfun=sim, fitfun=fit
+sessionInfo()
+
+print(try(simCIs(simfun=sim, fitfun=fit
 	, groupSize = 15, days=seq(3, 60, by=3)
 	, β0=10, β_treat=10, β_day=2
 	, sdint=3, sdslope=0.1, corr=0, sdres=2
-))
-
-cis <- map_dfr(1:nReps, simCIs, .id="sim", simfun=sim, fitfun=fit
-	, groupSize = 15, days=seq(3, 60, by=3)
-	, β0=10, β_treat=10, β_day=2
-	, sdint=3, sdslope=0.1, corr=0, sdres=2
-)
-
-summary(cis)
-
-## saveRDS(cis, "sims.rds")
-## cis <- readRDS("sims.rds")
-
-## summary(cis |> mutate_if(is.character, as.factor))
-
-## Check above
-treatmentTrue <- 10
-
-## We are only interested in “power” to detect the true effect direction
-print(cis
-	|> filter(vname=="treatmentexercise")
-	|> summarize(
-		toohigh=mean(low>treatmentTrue)
-		, toolow=mean(high<treatmentTrue)
-		, ci_width=mean(high-low)
-		, power = mean(low>0)
-	)
-)
+)))
